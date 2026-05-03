2ce3f9322b8d204aceb5846fe1a11cc6dd9b96a3
Alex Henriques <henriquesbr@gmail.com>
2026-05-03 18:46:36 -0300

    created base class for all DSP to provide unified interface

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -65,14 +65,43 @@ public:
         END_OF_LIST
     };
     
+    using DSP_Order = std::array<DSP_Option, static_cast<size_t>(DSP_Option::END_OF_LIST)>;
+    
 private:
     //==============================================================================
     // AHENRIQU: Started programming for Project13
     // https://www.youtube.com/watch?v=fmrLo9OF8bE&list=PLL4Pwd7fD7npBIpgp6Hwzod1PkjsbI0dW&index=54
-    // 25:08
-    juce::dsp::Phaser<float> phaser;
-    juce::dsp::Chorus<float> chorus;
-    juce::dsp::LadderFilter<float> overdrive, ladderFilter;
+    // 25:57
+    DSP_Order dspOrder;
+    
+    template<typename DSP>
+    struct DSP_Choice : juce::dsp::ProcessorBase
+    {
+        void prepare(const juce::dsp::ProcessSpec& spec) override
+        {
+            dsp.prepare(spec);
+        };
+        
+        void process (const juce::dsp::ProcessContextReplacing<float>& context) override
+        {
+            dsp.process(context);
+        };
+        
+        void reset() override
+        {
+            dsp.reset();
+        };
+        
+        DSP dsp;
+    };
+    
+    DSP_Choice<juce::dsp::DelayLine<float>> delay;
+    DSP_Choice<juce::dsp::Phaser<float>> phaser;
+    DSP_Choice<juce::dsp::Chorus<float>> chorus;
+    DSP_Choice<juce::dsp::LadderFilter<float>> overdrive, ladderFilter;
+    
+    using DSP_Pointers = std::array<juce::dsp::ProcessorBase*,
+        static_cast<size_t>(DSP_Option::END_OF_LIST)>;
     
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Project13AudioProcessor)
